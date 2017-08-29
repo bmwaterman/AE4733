53b134e016785b232aa8ba05388fcbd91f67fc59
Keshuai Xu <i@xks.im>
2017-08-29 15:47:38 -0400

    add titles for questions

--- HW_1.m
+++ HW_1.m
@@ -1,7 +1,9 @@
+%% 1a
 TAS = 70; %m/s
 g = 10; %m/s^2
 bank = 20*pi/180; %radians
 syms r;
 turn_radius = vpasolve(bank-atan(((TAS^2)/r)/g),r,1000); % m
-time = vpa(((90*pi/180)*turn_radius)/TAS); % s
+time = vpa(((90*pi/180)*turn_radius)/TAS) % s
 
+%% 1b
