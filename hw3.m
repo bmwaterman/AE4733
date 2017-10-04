7d38e4ebbd3da8569d43d866ca8bd62255574a7b
Bailey Waterman <31453418+bmwaterman@users.noreply.github.com>
2017-10-04 17:54:58 -0400

    problem 6

--- hw3.m
+++ hw3.m
@@ -45,14 +45,31 @@ xi = C \ z                                                              % the le
 
 %% problem 7.1
 clear variables
-data = csvread('hw3q7.csv',1,0)';
-plot(data(1, :),data(2:end, :))
+data = csvread('hw3q7.csv',1,0);
+plot(data(:, 1),data(:, 2:end))
 
 %% problem 7.2
 
 y0 = [zeros(3,1);zeros(3,1); zeros(3,1)];
-p7_data.time_stamp = data(1, :)';
-p7_data.accel_readings = 
-[t_sim, y_sim] = ode45(@(t,y) ins_fun(t,y,p4_data), p4_data.time_stamp, y0);
+p7_data.time_stamp = data(:, 1)';
+p7_data.accel_readings = data(:, 2:4)';
+p7_data.gyro_readings = data(:, 7:-1:5)'; % csv data is roll pitch yaw
+[t_sim, y_sim] = ode45(@(t,y) ins_fun(t,y,p7_data), p7_data.time_stamp, y0);
 
+figure();
+plot3(y_sim(:,1),y_sim(:,2),y_sim(:,3));
+title('position trajectory')
+
+figure();
+plot(t_sim, y_sim(:,4:6));
+xlabel('time (sec)');
+ylabel ('velocity (m/s)');
+title('velocity');
+
+figure();
+plot(t_sim, rad2deg(y_sim(:,6:9)));
+xlabel('time (sec)');
+ylabel ('euler angles (deg)');
+title('euler angles');
+legend ('yaw(psi)','pitch(theta)','roll(phi)')
 
