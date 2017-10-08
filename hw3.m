a1829b349af1744f9b09c162e09a21347af49225
Keshuai Xu <i@xks.im>
2017-10-08 01:19:12 -0400

    fixed problem 6

--- hw3.m
+++ hw3.m
@@ -25,24 +25,22 @@ legend ('yaw(psi)','pitch(theta)','roll(phi)')
 
 %% problem 6
 p6_data = load ('hw3_p6.mat');
-% p6_temperature = p6_data.measurements(1:1500);
-% p6_angle_deg = p6_data(1501:3000);
-% p6_range = p6_data(3001:5000);
+
+% We scaled the m to km to fix bad scaling
 
 t0 = 288.16; % Kelvin
-a1 = -6.5e-3; % Kelvin/meter.
+a1 = -6.5; % Kelvin/km.
 
-N_temp = normrnd(0, 2, 1500,1);    % Normrnd function creates random numbers within the standard deviation
-N_angle = normrnd(0, 0.5, 1500, 1); % I made a 1500x1 matrix of random #s for temperature, a 1500x1 matrix with random #s 
-N_range = normrnd(0, 100, 2000, 1); % for angle and a 200x1 matrix with random #s for range
-
-N = [N_temp; N_angle; N_range;]; %I combined these three noise readings to form a single matrix
+% units in Kelvin, deg, km
+R = sparse(diag([2 * ones(1500,1); 0.5 * ones(1500,1); 0.1 * ones(2000,1)].^2));
 
 offset = [- t0 * ones(1500, 1); zeros(3500,1)];
-z = (p6_data.measurements + offset) - N;                                % I subtracted the noise from the measurements %% this is the part I'm not sure about beacause
-C = [ones(1500,1) * a1; ones(1500,1) * 180 / (pi * 1e4); ones(2000,1)]; % I'm not sure adjusting the equation he gave us in class like this will still result in 
-xi = C \ z                                                              % the least squares estimate formula
+scale = [ones(3000,1); 1e-3 * ones(2000,1)];
+z = p6_data.measurements .* scale + offset;
+C = sparse([a1 * ones(1500,1); ones(1500,1) * 180 / (pi * 1e1); ones(2000,1)]);
 
+xi_hat = inv(C' * inv(R) * C ) * C' * inv(R) * z; % km
+xi_hat_m = xi_hat * 1e3 % m
 %% problem 7.1
 clear variables
 data = csvread('hw3q7.csv',1,0);
