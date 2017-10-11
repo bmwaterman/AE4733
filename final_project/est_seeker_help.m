51a55cf2456df9045a0bf163e38b58e8cd98d3bb
Keshuai Xu <i@xks.im>
2017-10-11 13:43:17 -0400

    fixed atan2

--- final_project/est_seeker_help.m
+++ final_project/est_seeker_help.m
@@ -12,8 +12,8 @@ alpha_dot_fun = @(in1)(in1(1,:).*in1(5,:)-in1(4,:).*in1(2,:))./(in1(1,:).^2+in1(
 beta_dot_fun =  @(in1)1.0./sqrt((in1(1,:).^2+in1(2,:).^2)./(in1(1,:).^2+in1(2,:).^2+in1(3,:).^2)).*1.0./(in1(1,:).^2+in1(2,:).^2+in1(3,:).^2).^(3.0./2.0).*(in1(1,:).^2.*in1(6,:)+in1(2,:).^2.*in1(6,:)-in1(4,:).*in1(1,:).*in1(3,:)-in1(5,:).*in1(2,:).*in1(3,:));
 v_c_fun = @(in1)-(in1(4,:).*in1(1,:)+in1(5,:).*in1(2,:)+in1(6,:).*in1(3,:)).*1.0./sqrt(in1(1,:).^2+in1(2,:).^2+in1(3,:).^2);
 
-alpha = atan2(p_and_p_dot(2), p_and_p_dot(1));
-% alpha = atan(p_and_p_dot(2) / p_and_p_dot(1));
+% alpha = atan2(p_and_p_dot(2), p_and_p_dot(1));
+alpha = atan(p_and_p_dot(2) / p_and_p_dot(1));
 beta = asin(p_and_p_dot(3) / norm(p_and_p_dot(1:3)));
 
 alpha_dot = alpha_dot_fun(p_and_p_dot);
