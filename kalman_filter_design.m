c7f5822fdfcb9658530e408072b05d1a29c1d495
Cosine <urillx@gmail.com>
2017-10-11 03:57:03 -0400

    EKF works!

--- kalman_filter_design.m
+++ kalman_filter_design.m
@@ -17,81 +17,54 @@ R_i_a = [cos(psi) sin(psi) 0;
          -sin(psi) cos(psi) 0;
          0 0 1];
 
-%% get yaw from mag
-% mag_e = [mag_x mag_y mag_z]';
-% mag_a = R_a_b' * R_b_e' * mag_e;
-% psi_mag = atan2(mag_a(2), mag_a(1))
 
-mag_i = [1 0 0]';
-mag_e = R_b_e * R_a_b * R_i_a * mag_i;
+%%
+H_321 = @(theta, phi) [-sin(theta) 0 1; 
+    sin(phi)*cos(theta) cos(phi) 0;
+    cos(phi)*cos(theta) -sin(phi) 0];
 
-%% get psid thetad phid from gyro
-% omega_ie_i = rotational_kinematics(0, [psi theta phi]', [gyro_z gyro_y gyro_x]')
-% psid_gyro = omega_ie_i(1);
-% thetad_gyro = omega_ie_i(2);
-% phid_gyro = omega_ie_i(3);
-omega_ie_e = R_b_e * R_a_b * [0 0 psid]' + R_b_e * [0 thetad 0]' + [phid 0 0]'
-
-%% use gravity to keep track of the orientation...
-acc_e = R_b_e * R_a_b * R_i_a * [0 0 1]';
-
-%% EKF
-x = [psi theta phi psid thetad phid]';
-
-f = x + dt * [x(4:6); 0; 0; 0];
-F = jacobian(f, x)
-
-% z = [gyro_x gyro_y gyro_z mag_x mag_y mag_z]';
-h = [omega_ie_e; mag_e; acc_e];
-H = jacobian(h, x)
+x = [psi theta phi]';
+u = [gyro_x gyro_y gyro_z dt]';
+f_expr = x + (H_321(theta, phi) \ u(1:3, :)) * u(4, :);
+h_expr = eye(3) * x;
+F_expr = jacobian(f_expr, x);
+H_expr = jacobian(h_expr, x);
 
 %%
 close all
 
-n_states = 6;
-n_measurements = 9;
-x_prev = zeros(6,1);
-u_prev = 0.5;
-P_prev = 0;
-Q = diag(ones(n_states,1)) * 0.01;
-R = diag(ones(n_measurements,1)) * 0.01;
+n_states = 3;
+n_measurements = 3;
+x_prev = [0 0 pi]';
+u_prev = [0 0 0 0.02]';
+P_prev = eye(n_states);
+Q = eye(n_states) * 1e-2;
+R = eye(n_measurements,1) * 1e-0;
 
-data = csvread('hw3q7.csv',1,0);
+data = csvread('hw3q7_2.csv',1,0);
 time_stamp = data(:, 1)';
-acc_readings = data(:, 2:4)';
-gyro_readings = data(:, 5:7)'; % csv data is roll pitch yaw
-mag_readings = data(:, 8:10)';
+% acc_readings = data(:, 2:4)';
+gyro_readings = data(:, 5:7)';
+% mag_readings = data(:, 8:10)';
+heading = deg2rad(data(:, 13:-1:11)');
+dt = [0.02, diff(time_stamp)];
 
-%hack
-mag_readings(3, :) = zeros(1, size(mag_readings,2));
+z_traj = heading;
+u_traj = [gyro_readings; dt];
+f_fun = matlabFunction(f_expr, 'Vars', {x,u});
+F_fun = matlabFunction(F_expr, 'Vars', {x,u});
+h_fun = matlabFunction(h_expr, 'Vars', {x});
+H_fun = matlabFunction(H_expr, 'Vars', {x});
 
-normalize = @(v) v/norm(v);
+x_traj = zeros(n_states, size(z_traj,2));
 
-for col = 1:size(mag_readings,2)
-    mag_readings(:, col) = normalize(mag_readings(:, col));
-end
-
-for col = 1:size(acc_readings,2)
-    acc_readings(:, col) = normalize(acc_readings(:, col));
-end
-
-z = [gyro_readings; mag_readings; acc_readings];
-f_fun = matlabFunction(f, 'Vars', {x,dt});
-F_fun = matlabFunction(F, 'Vars', {x,dt});
-h_fun = matlabFunction(h, 'Vars', {x});
-H_fun = matlabFunction(H, 'Vars', {x});
-
-x_traj = zeros(6, size(z,2));
-
-for t = 1:size(z,2)
-    [ x_estimate, P_estimate ] = kalman_sensor_fusion(x_prev, u_prev, P_prev, f_fun, F_fun, h_fun, H_fun, z(:, t), Q, R);
+for t = 2:size(z_traj,2)
+    [ x_estimate, P_estimate ] = kalman_sensor_fusion(x_prev, u_traj(:, t-1), P_prev, f_fun, F_fun, h_fun, H_fun, z_traj(:, t), Q, R);
     x_traj(:, t) = x_estimate;
     x_prev = x_estimate;
     P_prev = P_estimate;
 end
 
 plot(time_stamp, x_traj(1:3,:))
-figure();
-plot(time_stamp, x_traj(4:6,:))
 
 
